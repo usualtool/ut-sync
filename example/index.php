84f1421170bdda28a457941bcd8a8e29d7f83574
黄豆 <292951110@qq.com>
2025-12-28 19:59:50 +0800

    Update index.php

--- example/index.php
+++ example/index.php
@@ -1,16 +1,18 @@
 <?php
 use usualtool\Sync\Sync;
-$local = '/data/images';
-$remote = '/data/images';
+$local = APP_ROOT . '/assets';
+$remote = '/www/wwwroot/ut6/app/assets';
 $servers = [
     [
-        'host' => '192.168.1.12',
-        'user' => 'syncuser',
-        'password' => 'your_strong_password_here'//参数改privateKey值改file_get_contents('/home/main/.ssh/sync_key')
+        'host' => '172.168.0.1',
+        'user' => 'root',
+        'password' => '123456'
+        // 如果要用私钥：'privateKey' => file_get_contents('/home/main/.ssh/sync_key')
     ]
+    //可以多组从服务器同步
 ];
-foreach ($servers as $config) {
-    echo "\n正在同步到 {$config['host']}...\n";
+foreach($servers as $config) {
+    echo "正在同步到 {$config['host']}...<br/>";
     try {
         $sync = new Sync(
             $config['host'],
@@ -20,15 +22,21 @@ foreach ($servers as $config) {
             $config['privateKey'] ?? null
         );
         $sync->SetMaxRetries(3, 2);
-        $result = $sync->SyncTo($local, $remote, enableDelete: false);
-        echo "同步完成: 上传 {$result['uploaded']} 个文件\n";
+        $sync->setLogger(function ($level, $message) {
+            if ($level === 'INFO' && strpos($message, '上传: ') === 0) {
+                $relPath = substr($message, strlen('上传: '));
+                echo "{$relPath} 同步成功<br/>";
+            }
+        });
+        $result = $sync->SyncTo($local, $remote, false);
+        echo "同步完成: 共上传 {$result['uploaded']} 个文件<br/>";
         if (!empty($result['errors'])) {
-            echo "以下操作出错:\n";
+            echo "以下操作出错:<br/>";
             foreach ($result['errors'] as $err) {
-                echo "  - $err\n";
+                echo "  - $err<br/>";
             }
         }
     } catch (\Exception $e) {
-        echo "❌ 同步失败 ({$config['host']}): " . $e->getMessage() . "\n";
+        echo "同步失败 ({$config['host']}): " . $e->getMessage() . "<br/>";
     }
 }
